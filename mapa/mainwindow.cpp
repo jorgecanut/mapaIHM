4afa163d1304f828f269dfa1051adea39bfcd8b9
Mvaovil <mvaovil@upv.edu.es>
2025-11-18 18:27:15 +0100

    conexiones entre las páginas de inicio de sesión y registro así como mostrar contraseña

--- mapa/mainwindow.cpp
+++ mapa/mainwindow.cpp
@@ -11,7 +11,29 @@ MainWindow::MainWindow(QWidget *parent)
     // Conecta el click de label_3 para cambiar de página
     connect(ui->label_3, &ClickableLabel::clicked, this, [this]() {
         ui->stackedWidget->setCurrentWidget(ui->page);
+
+
     });
+    connect(ui->label_10, &ClickableLabel::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->page_2);
+
+
+    });
+    connect(ui->checkBox_2, &QCheckBox::toggled, this, [this](bool checked) {
+        if (checked) {
+            ui->lineEdit_6->setEchoMode(QLineEdit::Normal);
+        } else {
+            ui->lineEdit_6->setEchoMode(QLineEdit::Password);
+        }
+    });
+    connect(ui->checkBox_3, &QCheckBox::toggled, this, [this](bool checked) {
+        if (checked) {
+            ui->lineEdit_3->setEchoMode(QLineEdit::Normal);
+        } else {
+            ui->lineEdit_3->setEchoMode(QLineEdit::Password);
+        }
+    });
+
 }
 
 MainWindow::~MainWindow()
