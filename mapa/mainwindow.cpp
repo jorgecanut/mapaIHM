8098e3a65e96cd9ec85c8efa8a6d66ee9181b223
Mvaovil <mvaovil@upv.edu.es>
2025-11-18 12:54:23 +0100

    Register your Label3 conectada para cambio de página

--- mapa/mainwindow.cpp
+++ mapa/mainwindow.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "login_register.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Conecta el click de label_3 para cambiar de página
+    connect(ui->label_3, &ClickableLabel::clicked, this, [this]() {
+        ui->stackedWidget->setCurrentWidget(ui->page);
+    });
 }
 
 MainWindow::~MainWindow()
