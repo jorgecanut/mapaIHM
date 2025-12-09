f27ccf7676eec201fc8786b9e14c08d6e55ac3b5
Jorge Canut <jorgecaru3@gmail.com>
2025-12-09 10:44:37 +0100

    zoom

--- mapa/mainwindow.cpp
+++ mapa/mainwindow.cpp
@@ -14,3 +14,21 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::applyZoom(double factor){
+    // factor > 1 acerca, factor < 1 aleja
+    double newScale = escalado * factor;
+    const double minScale = 0.1;
+    const double maxScale = 1;
+
+    if (newScale < minScale){
+        factor = minScale / escalado;
+        newScale = maxScale;
+    }
+    else if (newScale > maxScale){
+        factor = maxScale / escalado;
+        newScale = maxScale;
+    }
+    view->scale(factor, factor);
+    escalado = newScale;
+}
