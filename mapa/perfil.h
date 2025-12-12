63afb2aaca835d9d2caf5e0a4a4c744d0ebd79b8
Vazesp <spedrazavazquez@gmail.com>
2025-12-12 20:20:43 +0100

    añadido el error de fecha

--- mapa/perfil.h
+++ mapa/perfil.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef PERFIL_H
 #define PERFIL_H
 
@@ -48,54 +47,3 @@ signals:
 };
 
 #endif // PERFIL_H
-=======
-#ifndef PERFIL_H
-#define PERFIL_H
-
-#include <QMainWindow>
-#include <QLabel>
-#include <QLineEdit>
-#include "mainwindow.h"
-
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class Perfil;
-}
-QT_END_NAMESPACE
-
-class Perfil : public QMainWindow
-{
-    Q_OBJECT
-
-public:
-    explicit Perfil(QWidget *parent = nullptr);
-    ~Perfil();
-
-private:
-    Ui::Perfil *ui;
-    bool validEmail;
-    bool validPassword;
-    bool validRepPassword;
-    bool validAge;
-
-    void manageError(QLabel *errorLabel, QLineEdit *edit, bool &flag);
-    void manageCorrect(QLabel *errorLabel, QLineEdit *edit, bool &flag);
-
-    void showErrorMessage(QLabel *errorLabel, QLineEdit *edit);
-    void hideErrorMessage(QLabel *errorLabel, QLineEdit *edit);
-
-    void checkEmail();
-    void checkPassword();
-    void checkDate();
-
-    void updateAcceptEnabled();
-
-private slots:
-    void onEmailEditingFinished();
-
-signals:
-    void clicked();
-};
-
-#endif // PERFIL_H
->>>>>>> 07a731ffc7106fa31e1d8e797eff888259dc0016
